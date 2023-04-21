67cde49fd7e3b8df44edac32b75691d699008c9e
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-04-21 16:43:23 +0200

    add style card

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -16,28 +16,49 @@ export default {
 
 <template>
     
-    <section v-for="(items, title) in store.allResults" class="py-5">
-        <div class="container">
-            <h2>{{ title }}</h2>
+    <section v-for="(items, title) in store.allResults" class="pb-5">
+        <div class="container-fluid px-5 mx-5">
+            <h2 class="text-uppercase text-center">{{ title }}</h2>
 
-            <div v-if="items" class="row row-cols-4">
-                <div v-for="item in items.results">
-                    <div class="card">
-                        <img :src="`https://image.tmdb.org/t/p/w780${item.poster_path}`" alt="">
+            <div v-if="items" class="row g-5">
+                
+                    <div class="cards col-4 py-5" v-for="item in items.results">
+
+                        <div class="front">
+                            <img :src="`https://image.tmdb.org/t/p/w780${item.poster_path}`" alt="">
+                        </div>
+
+                        <div class="back">
+
+                            <div v-if="title === 'movies'">
+                                <h3>
+                                    titolo: {{ item.title }}
+                                </h3>
+                                <h5>
+                                    titolo originale:{{ item.original_title }}
+                                </h5>
+                                
+                            </div>
+                            <div v-else>
+                                <h3>
+                                    {{ item.name }}
+                                </h3>
+                                <h5>
+                                    {{ item.original_name }}
+                                </h5>
+                            </div>
+
+                            <span>voto: {{ item.vote_average }}</span>
+                            <div class="my-1">
+                                <span>
+                                    <strong>Lingua:    <img :alt="item.original_language" width="32" :src="`https://cdn.ipregistry.co/flags/emojitwo/${item.original_language}.svg`"/></strong>
+                                </span>
+                                
+                            </div> 
+                        </div>
                     </div>
-                    <div v-if="title === 'movies'">
-                        {{ item.title }}
-                        {{ item.original_title }}
-                    </div>
-                    <div v-else>
-                        {{ item.name }}
-                        {{ item.original_name }}
-                    </div>
-                    <span>{{ item.vote_average }}</span>
-                    <div>
-                        <img :alt="item.original_language" width="32" :src="`https://cdn.ipregistry.co/flags/emojitwo/${item.original_language}.svg`"/>
-                    </div> 
-                </div>
+                    
+            
             </div>
         </div>
         
@@ -50,9 +71,65 @@ export default {
 <style lang="scss" scoped>
 section {
     background-color: #414141;
-    color: #fff;
+    color: #ffffff;
+    min-height: 45vh;
+
     h2 {
-        color: #000;
+        color: #eaeaea;
+        font-size: 4.5rem;
+        font-weight: bold;
     }
+
+    .cards {
+        position: relative;
+        max-width: 300px;
+        min-height: 500px;
+        left: 0;
+        top: 0;
+        transform-style: preserve-3d;
+        perspective: 600px;
+        transition: 0.5s;
+        margin-right: 0.75rem;
+        margin-bottom: 0.75rem;
+
+        img {
+            max-height: 100%;
+            object-fit: cover
+        }
+
+
+        .front {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            backface-visibility: hidden;
+            transform: rotateY(0deg);
+            transition: 0.5s;
+        }
+
+        &:hover .front {
+            transform: rotateY(-180deg);
+        }
+        .back {
+            border: 5px solid #dc1a28;
+            padding: 2.5rem 1rem;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: #1d1d1d;
+            backface-visibility: hidden;
+            transform: rotateY(180deg);
+            transition: 0.5s;
+        }
+
+        &:hover .back {
+            transform: rotateY(0deg);
+        }
+    }
+
 }
 </style>
